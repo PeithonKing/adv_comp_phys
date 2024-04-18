9a1b65fe6047d25ccc0507878d1ac10b2456d22f
PeithonKing <aritra.mukhopadhyay@niser.ac.in>
2024-04-18 15:19:56 +0530

    Add fitting functions for polynomial and custom basis in fitting.py

--- library/eigen.py
+++ library/eigen.py
@@ -1,36 +1,96 @@
-try: from library.matrix import Matrix, randmat, truncate_p
-except ImportError: from matrix import Matrix, randmat
-import math
+import numpy as np
 
 
-def get_eigen(A:Matrix, epsilon:float = 1e-6, seed:float = 0.1):
-    """Get the principal eigenvalue and eigenvectors of a matrix using the power method.
+# def get_eigen(A:Matrix, epsilon:float = 1e-6, seed:float = 0.1):
+#     """Get the principal eigenvalue and eigenvectors of a matrix using the power method.
+
+#     Args:
+#         A (Matrix): Matrix to be analyzed.
+#         epsilon (float, optional): Tolerance. Defaults to 1e-6.
+#         seed (float, optional): Initial guess. Defaults to 0.1.
+
+#     Returns:
+#         eigenvalue, eigenvector, iterations: Principal eigenvalue and eigenvector, and number of iterations.
+#     """
+#     precision = int(-math.log10(epsilon))
+#     x0 = randmat((len(A), 1), name="x0", precision=precision, seed=seed)
+
+#     z = x0
+#     i = 0
+#     lambda_old = 0
+#     while True:
+#         y = z
+#         z = A@y
+#         lambda_new = ((z.T()@x0)/(y.T()@x0)).mat[0][0]
+#         i += 1
+#         # print(f"{lambda_new = }")
+#         if abs(lambda_old-lambda_new)<=epsilon:
+#             break
+#         lambda_old = lambda_new
+    
+#     y = y.normalise()
+    
+#     y.name = "e-vector"
+#     return truncate_p(lambda_new, precision, str), y, i
+
+def power_method(matrix, tolerance=1e-6, max_iterations=100, seed=10):
+    """This code finds out the maximum eigenvalues and eigenvectors of a matrix
 
     Args:
-        A (Matrix): Matrix to be analyzed.
-        epsilon (float, optional): Tolerance. Defaults to 1e-6.
-        seed (float, optional): Initial guess. Defaults to 0.1.
+        matrix (array): given matrix
+        tolerance (float, optional): the tolerance. Defaults to 1e-6.
+        max_iterations (int, optional): the maximum no of iterations. Defaults to 1000.
 
     Returns:
-        eigenvalue, eigenvector, iterations: Principal eigenvalue and eigenvector, and number of iterations.
+        eigvalue, eigvector, iterations: Maximum eigenvalue, eigenvector, and number of iterations taken
     """
-    precision = int(-math.log10(epsilon))
-    x0 = randmat((len(A), 1), name="x0", precision=precision, seed=seed)
-    
-    z = x0
-    i = 0
-    lambda_old = 0
-    while True:
-        y = z
-        z = A@y
-        lambda_new = ((z.T()@x0)/(y.T()@x0)).mat[0][0]
-        i += 1
-        # print(f"{lambda_new = }")
-        if abs(lambda_old-lambda_new)<=epsilon:
+    matrix = matrix.copy()
+    n = matrix.shape[0]
+    np.random.seed(seed)
+    evec = np.random.rand(n)
+    evec = evec / np.linalg.norm(evec)
+
+    for i in range(max_iterations):
+        evec_new = matrix @ evec
+        e_val = evec_new @ evec
+        evec_new = evec_new / np.linalg.norm(evec_new)
+
+        if np.linalg.norm(evec_new - evec) < tolerance:
             break
-        lambda_old = lambda_new
+
+        evec = evec_new
+
+    return e_val, evec_new, i
+
+def _QR_factorize(matrix):  # internal function not to be used outside
+    Q_matrix = np.zeros(matrix.shape)
+    R_matrix = np.zeros(matrix.shape)
+
+    for i in range(matrix.shape[1]):
+        minus = (matrix[:, i] @ Q_matrix[:, :i]) * Q_matrix[:, :i]
+        v_i = matrix[:, i] - minus.sum(axis=1)
+        Q_matrix[:, i] = v_i / np.linalg.norm(v_i)
+        R_matrix[:i+1, i] = matrix[:, i] @ Q_matrix[:, :i+1]
     
-    y = y.normalise()
-    
-    y.name = "e-vector"
-    return truncate_p(lambda_new, precision, str), y, i
+    return Q_matrix, R_matrix
+
+
+def qr_method(A, tolerance = 1e-6, max_iteration=100):
+    """This code finds out the eigenvalues of a matrix using QR decomposition
+
+    Args:
+        A (np.array): given matrix
+        tolerance (float, optional): Tolerance. Defaults to 1e-6.
+        max_iteration (int, optional): Maximum number of iterations. Defaults to 100.
+
+    Returns:
+        np.array, int: Eigenvalues and number of iterations
+    """
+    for i in range(max_iteration):
+        Q, R = _QR_factorize(A)
+        new_A = R @ Q
+        if np.linalg.norm(A - new_A) < tolerance:
+            break
+        A = new_A
+
+    return np.diag(A), i
--- library/fitting.py
+++ library/fitting.py
@@ -1,3 +1,4 @@
+import numpy as np
 try:
     from library.matrix import Matrix
     import library.matrix as m
@@ -35,7 +36,7 @@ def linear_fit(xs, ys, sigma = None):
     
     return [a2, a1], [Delta_a2_sq**0.5, Delta_a1_sq**0.5], r2
 
-def polynomial_fit(x, y, n):
+def polynomial_fit(x, y, n):  # old function, don't use
     A = m.ones([n, n], "A", 3)
     nem = [[len(x)]+[(x**j).sum() for j in range(1, n)]]
     A[0] = nem
@@ -55,3 +56,56 @@ def polynomial_fit(x, y, n):
     a = backward_propagation(L.T(), y1)
 
     return a
+
+# def mypolyfit(x_, y, sigma=None, degree=1):
+#     degree += 1
+#     if sigma==None: sigma = np.ones(len(y))
+#     x = np.zeros((x_.shape[0], degree))
+    
+#     # preparing x matrix: each column is 1, x, x^2, x^3, ..., x^n
+#     for i in range(degree):
+#         x[:, i] = np.array(x_)**i
+    
+#     # Theory:
+#     # the Loss function is defined as: L = ((X@W - Y)/sigma)^2
+#     # where W is the unknown parameters to be found
+#     # our job is to minimize L by changing W
+#     # at the optimum W, dL/dW = 0 (dL/dW is a vector where each element is dL/dW_i)
+#     # dL/dW = 2*X.T @ ((X@W - Y)/sigma^2) = 0
+#     # X.T @ (X@W - Y)/sigma^2 = 0
+#     # X.T/sigma^2 @ X @ W = X.T/sigma^2 @ Y
+#     # A @ W = B, where A = X.T/sigma^2 @ X and B = X.T @ Y/sigma^2
+#     # now we just need to solve this linear system of equations
+    
+#     A = (x.T / sigma**2) @ x
+#     B = x.T @ (y / sigma**2)
+#     return np.linalg.solve(A, B), A
+
+def mypolyfit(x_, y, basis=None, sigma=None, degree=1):
+    degree += 1
+    if sigma==None: sigma = np.ones(len(y))
+    if basis == None:
+        def funcs(d): return lambda x: x**d
+        basis = [funcs(i) for i in range(degree)]
+    else:
+        assert len(basis) == degree, "basis must have the same length as degree+1"
+    x = np.zeros((x_.shape[0], degree))
+    
+    # preparing x matrix: each column is phi_0(x), phi_1(x), phi_2(x), ..., phi_n(x)
+    for i in range(degree):
+        x[:, i] = basis[i](x_)
+    
+    A = (x.T / sigma**2) @ x
+    B = x.T @ (y / sigma**2)
+
+    return np.linalg.solve(A, B), A
+
+def pass_all_points(x_, y):
+    degree = len(y)
+    x = np.zeros((x_.shape[0], degree))
+    
+    # preparing x matrix: each column is 1, x, x^2, x^3, ..., x^n
+    for i in range(degree):
+        x[:, i] = np.array(x_)**i
+    
+    return np.linalg.solve(x, y), x
